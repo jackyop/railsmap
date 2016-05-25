70247d071bc87344cba2ff77fac1f937d359d3f2
carlo <carlor@uninorte.edu.co>
2016-05-25 14:18:40 -0500

    Two

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,14 @@
 class PagesController < ApplicationController
 	def home
+
 	end
 	def about
 	end
 	def contact
 	end
 	def map
+	end
+	def option
+  		render text: '', content_type: 'text/plain'
 	end
 end
